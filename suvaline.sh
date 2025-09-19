bd267b4fe2b3c3bd195b40590415d88561089a5a
Kerstin Lindh <kerstindorrit@hkhk.edu.ee>
2025-09-19 10:56:09 +0300

    1234

---
+++ suvaline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# See on minu esimene bash'i skript, mis teeb midagi
+
+TERVITUS="See on tervitus AI poolt"
+
+echo $TERVITUS
+echo "Sina oled  $(whoami)"
+echo "Tore oli sinuga tuttavaks saada!"
